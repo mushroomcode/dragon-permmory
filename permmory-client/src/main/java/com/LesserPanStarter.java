5e2f0f71aa64447ebf084035d537568a1afc31d6
joyyue <live_orevil@126.com>
2024-08-11 15:35:18 +0800

    @Committed starter文件

--- permmory-client/src/main/java/com/LesserPanStarter.java
+++ permmory-client/src/main/java/com/LesserPanStarter.java
@@ -1,15 +1,13 @@
 package com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.DriverManager;
-
 @SpringBootApplication
+@MapperScan("com.mapper")
 public class LesserPanStarter {
-
     public static void main(String[] args) {
         SpringApplication.run(LesserPanStarter.class);
     }
-
 }
