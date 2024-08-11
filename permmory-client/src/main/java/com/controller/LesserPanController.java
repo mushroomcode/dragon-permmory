8ec4b1d7eb04a516466eff683b5caa1ed1234f8c
joyyue <live_orevil@126.com>
2024-08-11 15:38:37 +0800

    @Committed starter文件

--- permmory-client/src/main/java/com/controller/LesserPanController.java
+++ permmory-client/src/main/java/com/controller/LesserPanController.java
@@ -1,23 +1,49 @@
 package com.controller;
 
+import com.mapper.LesserFileMgrMapper;
+import com.pojo.result.LesserFileInfoResult;
+import com.services.LesserPanUploadFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RestController
-@RequestMapping("/")
+@Controller
+@RequestMapping("/root")
 public class LesserPanController {
 
-    @RequestMapping("/fileUpload")
-    public void lesserFileUploader (MultipartFile file) {
-        try {
-            file.getInputStream();
+    @Autowired
+    private LesserPanUploadFileService lesserPanUploadFileService;
 
-        } catch (IOException e) {
+    @Autowired
+    private LesserFileMgrMapper mgrMapper;
+
+    @RequestMapping(value = "/fileUpload", method = RequestMethod.GET)
+    @ResponseBody
+    public void lesserFileUploader () {
+        try {
+//            file.getInputStream();
+            lesserPanUploadFileService.fileUpload(null);
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @RequestMapping(value = "/healtchk", method = RequestMethod.GET)
+    @ResponseBody
+    public String lesserHealthChk () {
+        return "it's ok!!!";
+    }
+
+    @RequestMapping(value = "/fileId", method = RequestMethod.GET)
+    @ResponseBody
+    public LesserFileInfoResult qryByLesserFileId (@RequestParam("fileId") String fileId) {
+        return mgrMapper.qryByFileId(fileId);
     }
 
 }
