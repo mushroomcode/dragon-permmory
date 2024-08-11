a9d0f26244e800c6054ff30b832a7482207a07db
joyyue <live_orevil@126.com>
2024-08-11 15:46:05 +0800

    @Committed starter文件

---
+++ permmory-client/src/main/java/com/pojo/result/LesserFileInfoResult.java
@@ -0,0 +1,24 @@
+package com.pojo.result;
+
+public class LesserFileInfoResult {
+
+    private String fileId;
+
+    private String fileName;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
