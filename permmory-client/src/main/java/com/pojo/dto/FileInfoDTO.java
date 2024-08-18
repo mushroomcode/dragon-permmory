fcf38a404860011e73f7d5020858ec42d5f0d60b
joyyue <live_orevil@126.com>
2024-08-18 13:14:14 +0800

    @Committed 文件上传模块

---
+++ permmory-client/src/main/java/com/pojo/dto/FileInfoDTO.java
@@ -0,0 +1,74 @@
+package com.pojo.dto;
+
+public class FileInfoDTO {
+
+    private Long fileId;
+
+    private String fileName;
+
+    private byte status;
+
+    private String uploadUser;
+
+    private String uploadTime;
+
+    private String type;
+
+    private String filePath;
+
+    public Long getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Long fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(byte status) {
+        this.status = status;
+    }
+
+    public String getUploadUser() {
+        return uploadUser;
+    }
+
+    public void setUploadUser(String uploadUser) {
+        this.uploadUser = uploadUser;
+    }
+
+    public String getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(String uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
