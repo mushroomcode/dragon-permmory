e17e2bbc29c616a783123fa4b75f8bc60cfd054a
joyyue <live_orevil@126.com>
2025-01-26 15:24:08 +0800

    @Committed 文件上传模块

---
+++ permmory-clothes/src/main/java/com/pojo/param/ClothesNode.java
@@ -0,0 +1,18 @@
+package com.pojo.param;
+
+/**
+ * 衣物节点
+ */
+public class ClothesNode {
+    // 上衣-0（01-T恤，），下衣-1（），鞋子-2（），配饰-3（）
+    private String type;
+
+    private String coler;
+
+    private String name;
+    // 使用次数（天）
+    private Short usedTime;
+
+
+
+}
