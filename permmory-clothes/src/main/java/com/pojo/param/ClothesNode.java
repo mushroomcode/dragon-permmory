1324c2f84247e3e1d6fa92a7a7fd3a277e1ea5c4
joyyue <live_orevil@126.com>
2025-01-26 15:30:07 +0800

    @Committed 文件上传模块

--- permmory-clothes/src/main/java/com/pojo/param/ClothesNode.java
+++ permmory-clothes/src/main/java/com/pojo/param/ClothesNode.java
@@ -13,6 +13,35 @@ public class ClothesNode {
     // 使用次数（天）
     private Short usedTime;
 
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
 
+    public String getColer() {
+        return coler;
+    }
+
+    public void setColer(String coler) {
+        this.coler = coler;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Short getUsedTime() {
+        return usedTime;
+    }
+
+    public void setUsedTime(Short usedTime) {
+        this.usedTime = usedTime;
+    }
 }
